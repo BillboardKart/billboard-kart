f04ce98d5f4165202bd69e4fccb944559b549493
Swapnil Narwade <swapnil.narwade3@gmail.com>
2026-07-17 06:42:13 +0530

    feat: exported prisma client to use within app

---
+++ packages/database/src/client.ts
@@ -0,0 +1,34 @@
+import "dotenv/config";
+
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+import { PrismaClient } from "./generated/prisma/client";
+
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL environment variable is not set.");
+}
+
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  (() => {
+    const pool = new Pool({
+      connectionString,
+    });
+
+    const adapter = new PrismaPg(pool);
+
+    return new PrismaClient({
+      adapter,
+    });
+  })();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
